c5f12f2786012e1259b1e60a14a7150dbc7d9984
Marcin Sola <marcinsolaonline@gmail.com>
2021-11-26 16:19:56 +0100

    Get list of campaigns from the contract

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
+import { useState, useEffect } from 'react';
 import { CampaignsList } from './components/CampaignsList';
 import { Header } from './components/Header';
+import { ethers } from 'ethers';
+const cryptStarter = require('./utils/abi/CryptStarter.json');
+const connectionData = require('./utils/connectionData.json');
 
 function App() {
+  const [campaignsList, setCampaignsList] = useState();
+
+  useEffect(() => {
+    const retrieveCampaigns = async () => {
+      const campaignsList = [];
+      const provider = new ethers.providers.AlchemyProvider('rinkeby');
+      const contract = new ethers.Contract(
+        connectionData.contractAddresses.rinkeby,
+        cryptStarter.abi,
+        provider
+      );
+
+      const numberOfCampaignsBN = await contract.getNumberOfCampaigns();
+      const numberOfCampaigns = ethers.BigNumber.from(
+        numberOfCampaignsBN._hex
+      ).toNumber();
+
+      if (numberOfCampaigns === 0) {
+        setCampaignsList([]);
+        return;
+      }
+
+      for (let i = 0; i < numberOfCampaigns; i++) {
+        const campaign = await contract.campaigns(i);
+        campaignsList.push(campaign);
+      }
+
+      setCampaignsList(campaignsList);
+    };
+
+    retrieveCampaigns();
+  }, []);
+
   return (
     <div>
       <Header />
-      <CampaignsList></CampaignsList>
+      <CampaignsList campaigns={campaignsList ?? []} />
     </div>
   );
 }
